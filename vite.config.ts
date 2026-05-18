4a3f1f9af1732192d88c8de556fc0c0ab10b03dc
jweese001 <jweese@bioscapedigital.com>
2026-05-17 23:19:09 -0400

    chore: deploy site to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import glsl from 'vite-plugin-glsl'
 
+const repoBase = '/khora-engine/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: repoBase,
   plugins: [
     react(),
     glsl()
